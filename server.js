9370878958132bae5a9721ccee3201fec7a88b6f
maybeitsai <harrymardika.ai@gmail.com>
2025-01-01 12:49:52 +0700

    refactor: reorganize server code and improve error handling

--- server.js
+++ server.js
@@ -112,10 +112,10 @@ app.use((err, req, res, next) => {
 });
 
 // Production setup
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "./dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "./dist", "index.html"));
 });
 
 // Start server
